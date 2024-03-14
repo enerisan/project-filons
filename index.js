1ccfc1f92563f7540fcd2fff4d51006e62ead8b3
JohaneDecamps <johane.decamps@gmail.com>
2024-03-14 16:59:06 +0100

    ..

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function createCard(imageUrl, textButton) {
 }
 
 /* EVENT */
-commencezButton.addEventListener("click", () => {
+  commencezButton.addEventListener("click", () => {
   logoMain.style.display = "none";
   commencezButton.style.display = "none";
   textRevez.innerHTML = "Quelles sont vos envies ?";
@@ -58,4 +58,137 @@ commencezButton.addEventListener("click", () => {
   }
 });
 
+const burgerButton = document.querySelector('#burger'); 
+
+document.addEventListener("change", function (event) {
+  const listeMenu = document.querySelector(".menu");
+  if (event.target.matches("#burger")) { 
+    if (event.target.checked){
+      listeMenu.style.display = "flex"; 
+    } else { 
+      listeMenu.style.display = "none";
+    }
+  }
+
+});
+
+
+
+
+
+
+
+const cityList =[
+  // Les Pieds dans l'eau 
+   { 
+       name : "saint-jean de luz",
+       picture : "imgp1/sanjeandeluz.jpg",
+       cat : "Lespiedsdansleau",
+       style : "500-1000",
+   },
+   {
+       name :"san sebastian",
+       picture: "imgp1/sansebastian.jpg",
+       cat : "Lespiedsdansleau",
+       style : "500-1000",
+   },
+   {
+       name : "santorin",
+       picture : "img1/grce.jpg",
+       cat : "Lespiedsdansleau",
+       style : "1000-2500",
+   },
+   {  name : "italie",
+      picture : "imgp1/italie.jpg",
+      cat : "Lespiedsdansleau",
+      style : "1000-2500",
+   },
+   {
+       name : "seychelles",
+       picture : "imgp1/seychelles.jpg",
+       cat : "Lespiecsdansleau",
+       style : "2500-10000",
+   },
+   {
+       name : "thailande",
+       picture : "imgp1/Thailande.jpg",
+       cat : "Lespiedsdansleau",
+       style : "2500-10000",
+   },
+   // A l'air frais 
+   {
+       name : "suisse",
+       picture : "imgp1/suisse.jpg",
+       cat : "Alairfrais",
+       style : "500-1000",
+   },
+  /* {
+       name : "...",
+       picture : "...",
+       cat : "Alairfrais",
+       style : "500-1000",
+   },*/
+   {
+       name : "lemontblanc",
+       picture : "imgp1/montblanc.jpg",
+       cat : "Alairfrais",
+       style : "1000-2500",
+   }, 
+   { 
+       name : "laponie",
+       picture : "imgp1/laponie.jp",
+       cat : "Alairfrais",
+       style : "1000-2500",
+   },
+   {
+       name : "monfuji",
+       picture : "imgp1/fuji.jpg",
+       cat : "Alairfrais",
+       style : "2500-10000",
+   },
+   {
+       name : "canada",
+       picture : "imgp1/canada.jpg",
+       cat : "Alairfrais",
+       style : "2500-1000",
+   },
+// A la ville 
+   { 
+       name : "paris",
+       picture : "imgp1/paris.jpg",
+       cat : "Alaville",
+       style : "500-1000",
+   },
+   { 
+       name : "dublin",
+       picture : "",
+       cat : "Alaville",
+       style : "500-1000",
+   },
+   {
+       name : "londres",
+       picture : "",
+       cat : "Alaville",
+       style : "1000-2500",
+   },
+   /* {
+       name : "",
+       picture : "",
+       cat : "Alaville",
+       style : "1000-2500",
+   }, */
+   { 
+       name : "newyork",
+       picture : "imgp1/ny2.jpg",
+       cat : "Alaville",
+       style : "2500-10000",
+   }, 
+   {
+       name : "tokyo",
+       picture : "",
+       cat : "Alaville",
+       style : "2500-10000",
+   },
+
+];
 
